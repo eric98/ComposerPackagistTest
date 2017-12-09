04d9e8991d793b35332e8efb2f66a3284662a39d
eric98 <ergare.17@gmail.com>
2017-12-09 21:49:32 +0100

    Sync

--- vendor/ergare17/hello-ng/src/Hello.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Hello {
-    public function hello()
-    {
-        $faker = Faker\Factory::create();
-        echo 'Hola '. $faker->name;
-    }
-}
--- vendor/ergare17/hello-ng/tests/test_hello.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require 'vendor/autoload.php';
-
-require 'src/Hello.php';
-
-$hello = new Hello();
-$hello->hello();
